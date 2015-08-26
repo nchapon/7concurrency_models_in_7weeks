215205eb5e49cbecd81f34337232054f590bdfcf
Nicolas Chapon <nchapon@gmail.com>
2015-08-27 01:06:54 +0200

    Add httpkit

---
+++ transcript-handler/project.clj
@@ -0,0 +1,11 @@
+(defproject transcript-handler "0.1.0-SNAPSHOT"
+  :description "Chap 3 / Day 3 / Web service"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :main transcript-handler.system
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [http-kit "2.1.18"]
+                 [compojure "1.3.4"]]
+  :profiles {:dev {:plugins []
+                   :dependencies [[reloaded.repl "0.1.0"]]
+                   :source-paths ["dev"]}})
---
+++ transcript-handler/src/transcript_handler/system.clj
@@ -0,0 +1,11 @@
+(ns transcript-handler.system
+  (:require [org.httpkit.server :refer [run-server]]))
+
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello HTTP!"})
+
+(defn -main [& args]
+  (run-server app {:port 8080}))
