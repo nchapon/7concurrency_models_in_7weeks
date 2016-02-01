0d57dd723bdc69e95f5335e8dca7124044ea00f6
Nicolas Chapon <nchapon@gmail.com>
2016-02-01 13:19:26 +0100

    Polling macro

--- CSP/polling/src/polling/core.clj
+++ CSP/polling/src/polling/core.clj
@@ -2,29 +2,24 @@
   (:require [clojure.core.async :as async :refer :all :exclude [map into reduce merge partition partition-by take]]))
 
 
-
-
 (defn poll-fn
   "doc-string"
   [interval action]
   (let [seconds (* 1000 interval)]
-    (go (while true)
-        (action)
-        (<! (timeout seconds)))))
-
-
-(def c (to-chan (iterate inc 0)))
-
-
-(poll-fn 10 #(println "Read " (<!! c)))
+    (go (while true
+          (action)
+          (<! (timeout seconds))))))
 
 
 
+(defmacro poll [interval & body]
+  `(let [seconds# (* ~interval 1000)]
+     (go (while true
+           (do ~@body)
+           (<! (timeout seconds#))))))
 
 
 
-
-
-
-
-
+(comment (poll 10
+                    (println "Polling at :" (System/currentTimeMillis))
+                    (println (<! ch))))
